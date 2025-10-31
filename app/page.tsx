4800ae02e0e9cf24cc8e097ca54851e0a8f579f9
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-10-31 11:40:52 +0530

    chore: replace dither bg with noice

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-import Dither from "@/components/Dither";
+import Noise from "@/components/Noise";
 import ShinyText from "@/components/ShinyText";
 
 export default function Home() {
   return (
     <div className="relative">
-      <div className="fixed inset-0 z-0">
-        <Dither enableMouseInteraction={false} />
+      <div className="fixed inset-0 z-0 bg-black">
+        <Noise 
+          patternSize={250}
+          patternAlpha={25}
+          patternRefreshInterval={2}
+        />
       </div>
       <div className="relative z-10 h-screen w-screen flex items-center justify-center">
         <ShinyText 
           text="CHALLA SRIHAS REDDY"
           className="font-figtree text-5xl md:text-6xl lg:text-8xl font-black tracking-widest text-center px-8 leading-none"
-          speed={3}
+          speed={5}
+          disabled={false}
         />
       </div>
     </div>
